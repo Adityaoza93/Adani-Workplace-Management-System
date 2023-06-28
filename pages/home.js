55683af5eaae76b7ed9ee3c5deb855c47c4fb702
Aditya Oza <86963278+Adityaoza93@users.noreply.github.com>
2023-06-28 09:46:04 +0530

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const home = () => {
     return (
-        <div>
+        <div className="my-4">
             <h1>Your Basic HTML Page</h1>
             <iframe
                 src="https://www.google.com/maps/d/u/0/embed?mid=1xJx-IKzGdu6qBxv_NPcD4DADfLrUJ8c&ehbc=2E312F"
-                width="2000"
-                height="1000"
+                width="4000"
+                height="2000"
             ></iframe>
         </div>
     )
